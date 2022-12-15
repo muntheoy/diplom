1f262f1e0cafd769538124b61ec3384ec8193cc0
Кузнецов Георгий Сергеевич <KuznetsovGS@ckba.local>
2022-12-15 09:05:25 +0600

    Добавлена темная тема страницы поиска

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,37 @@
 window.addEventListener('load', function () {
-    const scroll_btn = document.querySelector('#scroll_btn');
-    scroll_btn.addEventListener('click', () => {
-          const html = document.querySelector('html');
-          html.scrollTop = '0';
+    try {
+      const scroll_btn = document.querySelector('#scroll_btn');
+      scroll_btn.addEventListener('click', () => {
+            const html = document.querySelector('html');
+            html.scrollTop = '0';
+        });
+    
+      const html_element = document.querySelector('html');
+      window.addEventListener('scroll', () => {
+        if ( html_element.scrollTop > (html_element.offsetHeight/2) ) {
+          scroll_btn.classList.remove('hidden');
+        } else {
+          scroll_btn.classList.add('hidden');
+        };
       });
-  
-    const html_element = document.querySelector('html');
-    window.addEventListener('scroll', () => {
-      if ( html_element.scrollTop > (html_element.offsetHeight/2) ) {
-        scroll_btn.classList.remove('hidden');
-      } else {
-        scroll_btn.classList.add('hidden');
+
+
+      const toggleThemeBtn = document.querySelector('#toggle_theme');
+      if (toggleThemeBtn) {
+        toggleThemeBtn.addEventListener('click', (event) => {
+          event.preventDefault();
+          const currentTheme = localStorage.getItem('theme');
+          const bodyElement = document.querySelector('body');
+          if (currentTheme === 'dark') {
+            bodyElement.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+          } else {
+            bodyElement.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+          };
+        });
       };
-    });
+    } catch(error) {
+      console.error('Ошибка обрабочика: ', error);
+    };
 });
